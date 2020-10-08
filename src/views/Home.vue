5724f7b6c56fd3df67da1eb2919815d557ad73ff
woosukji <woosukji@snu.ac.kr>
2020-10-08 11:14:32 +0000

    MOD: added style

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,32 @@
 <template>
-  <div class="home">
+  <!-- <div class="home">
     <button @click="handleDashboard">Dashboard</button>
     <div class="search-soldier">
-      <input v-model.trim="searchKeyword" />
+      <input v-model.trim="searchKeyword" 
+        placeholder="훈련병 이름 검색" />
       <button
         @click="handleSearch"
         :disabled="searchKeyword === ''"
-        placeholder="훈련병 이름 검색"
       >
         Let's write!
       </button>
     </div>
-  </div>
+  </div> -->
+  <v-container id="home">
+    <v-btn @click="handleDashboard">
+      <span>Dashboard</span>
+    </v-btn>
+    <v-row justify="space-between">
+      <v-col cols="20" md="4">
+        <v-text-field v-model.trim="searchKeyword" label="훈련병 이름 검색" />
+      </v-col>
+      <v-col cols="12" md="4">
+        <v-btn @click="handleSearch" :disabled="searchKeyword === ''">
+          <span>Let's write!</span>
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -30,6 +45,7 @@ export default {
         name: "Write",
         params: { soldier: this.searchKeyword }
       });
+      this.searchKeyword = "";
     }
   }
 };
