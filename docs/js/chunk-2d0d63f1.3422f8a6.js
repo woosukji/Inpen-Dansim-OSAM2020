ee04bd397dacd94a4c0f6a12788c0d9b6bac547a
woosukji <woosukji@snu.ac.kr>
2020-10-08 11:36:16 +0000

    MOD: Run build & cleaned README

--- docs/js/chunk-2d0d63f1.3422f8a6.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0d63f1"],{7277:function(e,t,n){"use strict";n.r(t);var s=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},a=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"write"},[n("h1",[e._v("This is a Dashboard page")])])}],r=n("2877"),c={},i=Object(r["a"])(c,s,a,!1,null,null,null);t["default"]=i.exports}}]);
-//# sourceMappingURL=chunk-2d0d63f1.3422f8a6.js.map
